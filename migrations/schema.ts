fbf48baa9b4a1169d3e02d5b6523e855ad2c9c58
VV17CH3R <vvitcherv@gmail.com>
2023-10-14 13:03:20 +0300

    first commit 4

--- migrations/schema.ts
+++ migrations/schema.ts
@@ -1,6 +1,8 @@
 import { pgEnum, pgTable } from "drizzle-orm/pg-core"
 
 import { sql } from "drizzle-orm"
+
+
 export const keyStatus = pgEnum("key_status", ['expired', 'invalid', 'valid', 'default'])
 export const keyType = pgEnum("key_type", ['stream_xchacha20', 'secretstream', 'secretbox', 'kdf', 'generichash', 'shorthash', 'auth', 'hmacsha256', 'hmacsha512', 'aead-det', 'aead-ietf'])
 export const factorStatus = pgEnum("factor_status", ['verified', 'unverified'])
